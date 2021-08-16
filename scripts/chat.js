85989598fe341e132c44b3d0e1973a1280ddf3a9
vitwits <vitwits@gmail.com>
2021-08-16 12:49:24 +0300

    added more app logic

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,21 +8,46 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection('chats');
+    this.unsub; //unsubscribe from room update when we change room
   }
-  async addChat(message){
-    //chat object
+  async addChat(message) {
+    //adding new chatroom for a specific user
     const now = new Date();
     const chat = {
-      message, 
+      message,
       username: this.username,
       room: this.room,
       created_at: firebase.firestore.Timestamp.fromDate(now)
     };
     //save the chat document
-    const response = await this.chats.addChat(chat);
+    const response = await this.chats.add(chat);
     return response;
   }
-}
 
-const chatroom = new Chatroom('gaming', 'shaun');
-console.log(chatroom);
+  getChats(callback) {  //realtime listener for new chats
+    this.unsub = this.chats  //this returns a function, so unsub becomes a function
+      .where('room', '==', this.room) // listen to changes on particular room
+      .orderBy('created_at')
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type === 'added') {
+            //update UI
+            callback(change.doc.data());
+          }
+        });
+      });
+  }
+  // update username functionality
+  updateName(username){
+    this.username = username;
+  }
+
+  // update chatroom
+  updateRoom(room){
+    this.room = room;
+    console.log(`current room is ${this.room}`);
+    if(this.unsub) {
+      this.unsub(); //unsubscribing from old room live changes
+    }
+  }
+}
---
+++ scripts/ui.js
@@ -0,0 +1,8 @@
+// render chat templates to the DOM
+//clear the list of chars (when room changes)
+
+class ChatUI {
+  constructor(list) {
+    this.list = list;
+  }
+}
